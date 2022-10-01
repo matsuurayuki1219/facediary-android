6ebc1251b9f9a02aee6c8995bf7c38d0af00447d
matsuura1219 <matsuurayuki1219@gmail.com>
2022-10-01 16:12:18 +0900

    fix: スプラッシュ画面をスタックから削除

--- app/src/main/java/jp/matsuura/facediary/ui/signUp/SignUpSuccessFragment.kt
+++ app/src/main/java/jp/matsuura/facediary/ui/signUp/SignUpSuccessFragment.kt
@@ -5,21 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import jp.matsuura.facediary.R
-import jp.matsuura.facediary.databinding.FragmentSinginBinding
-import jp.matsuura.facediary.databinding.FragmentSingupBinding
 import jp.matsuura.facediary.databinding.FragmentSingupSuccessBinding
-import jp.matsuura.facediary.ui.signUp.SignUpViewModel
-import kotlinx.coroutines.flow.onEach
-
-/**
- * サインアップ成功画面
- */
-
 @AndroidEntryPoint
 class SignUpSuccessFragment: Fragment(R.layout.fragment_singup_success) {
 
