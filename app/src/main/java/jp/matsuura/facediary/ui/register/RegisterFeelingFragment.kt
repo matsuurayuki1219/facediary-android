acacaf840c24e9cd119f34f47e2d98d60805908a
matsuura1219 <matsuurayuki1219@gmail.com>
2022-10-09 18:01:34 +0900

    refactor: 依存ライブラリの削除

--- app/src/main/java/jp/matsuura/facediary/ui/register/RegisterFeelingFragment.kt
+++ app/src/main/java/jp/matsuura/facediary/ui/register/RegisterFeelingFragment.kt
@@ -1,21 +1,10 @@
 package jp.matsuura.facediary.ui.register
 
-import android.Manifest
-import android.app.Activity.RESULT_OK
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
-import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.core.view.children
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -26,8 +15,6 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import jp.matsuura.facediary.R
-import jp.matsuura.facediary.common.extenstion.showConfirm
-import jp.matsuura.facediary.common.extenstion.toBitmap
 import jp.matsuura.facediary.databinding.FragmentRegisterFeelingBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.launchIn
--- app/src/main/java/jp/matsuura/facediary/ui/register/RegisterFeelingViewModel.kt
+++ app/src/main/java/jp/matsuura/facediary/ui/register/RegisterFeelingViewModel.kt
@@ -1,16 +1,8 @@
 package jp.matsuura.facediary.ui.register
 
-import android.graphics.Bitmap
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import jp.matsuura.facediary.common.extenstion.toBinary
-import jp.matsuura.facediary.data.datasource.CacheDir
-import jp.matsuura.facediary.usecase.CalculateEmotionUseCase
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
-import java.io.File
-import java.io.InputStream
 import javax.inject.Inject
 
 @HiltViewModel
